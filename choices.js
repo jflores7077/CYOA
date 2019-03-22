6547f879526b7363ef0ed20061b7e5b305e74f3d
nyang3006 <nyang3006@baysidehighschool.org>
2019-03-22 11:10:22 -0400

    Update choices.js

--- choices.js
+++ choices.js
@@ -1,9 +1,9 @@
 const abc123 = {
-  msg: "You're at the city",
+  msg: "The story starts out in NY at the museum of History in Manhattan. The protagonist finds a map which seems to be blank to the eyes of other who see it. He however, can see it because he has the power to be able to see maps which are blank to everyone else. He finds a route to Miami Fl. And at the top of the map he sees a familiar figure that looks like a trident 🔱. He then makes a sketch of that map since he can’t take the one from the Museum. This map has coordinates which leads to an unknown location in Miami. ",
   options:{
     a:{
       msg:"You found a coin",
-      info:"go outside",
+      info:"Go to the coordinates which were provided by the map.",
       options:{
         a:{
           msg:"the coin grew legs :o",
@@ -17,7 +17,7 @@ const abc123 = {
     },
     b:{
       msg:"you stayed inside",
-      info:"stay inside",
+      info:"Stay in NY and continue your archeologist career.",
       options:{
         a:{
           msg:"you died in your sleep :(",
